eac166e0feb18e0f689b172c0bd1fad81b216bf0
Monster <97206566+MonsterScripts@users.noreply.github.com>
2022-10-08 22:10:52 +0200

    Update Cleaner.bat

--- Cleaner.bat
+++ Cleaner.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDxgfyXCcSuTCLMV5uHv9tajrVoTWO0+fJzn3+XXcOUV7kS8Jc58gH5Ywc1eDU9eL0utPgsy+mwS4TzLPsST0w==
-::YAwzuBVtJxjWCl3EqQJhSA==
-::ZR4luwNxJguZRRnVrRBgeksGHWQ=
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDxgfyXCcSuTCLMV5uHv9tajrVoTWO0+fJzn2biLIe4W+AvhbZNN
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @Echo off
 goto check_Permissions
 :check_Permissions
